b413dc4e5193070c1773481a76d97b0f5551dff0
Fandy Barestu <fandy@softwareseni.com>
2019-06-29 22:15:14 +0700

    Deploy to heroku setup

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 const connectDB = require('./config/db');
+
+const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongooseDB
@@ -20,6 +22,16 @@ app.get('/', (req, res) => {
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/users', require('./routes/users'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    return res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
